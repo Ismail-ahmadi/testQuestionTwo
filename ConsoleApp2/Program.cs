124fb5ff5464cdb9df2b165cac4e1bdc4b6a7e7e
Ismail Ahmadi <43436401+Ismail-ahmadi@users.noreply.github.com>
2023-04-17 20:02:07 -0400

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿public class MyGenericClass<T>
+{
+    private T item;
+
+    public MyGenericClass(T item)
+    {
+        this.item = item;
+    }
+
+    public T GetItem()
+    {
+        return item;
+    }
+}
+public class Program
+{
+    static void Main(string[] args)
+    {
+        MyGenericClass<int> MyGenericClassName = new MyGenericClass<int>(42);
+        int item1 = MyGenericClassName.GetItem(); // item1 will be 42
+        Console.WriteLine(item1); 
+
+        MyGenericClass<string> MyGenericClassName2 = new MyGenericClass<string>("hello");
+        string item2 = MyGenericClassName2.GetItem(); // item2 will be "hello"
+        Console.WriteLine(item2);
+
+    }
+}
